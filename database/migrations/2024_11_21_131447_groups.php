b59ff854d7fb90307dff71acd1d1c1132f5fff18
Zuliet_zsp1 <julia.jedrzejewska@zsp1jarocin.edu.pl>
2024-11-21 14:21:48 +0100

    students migration #1

---
+++ database/migrations/2024_11_21_131447_groups.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('classes_day');
+            $table->string('classes_hour');
+            $table->unsignedBigInteger('id_teacher');
+            $table->foreign('id_teacher')->references('id')->on('teachers');
+            $table->unsignedBigInteger('id_location');
+            $table->foreign('id_location')->references('id')->on('locations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
