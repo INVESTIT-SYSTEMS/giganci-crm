6788687d21a0df1062b8cb118a108754c63d3751
Jakub Jańczak <jakub.janczak@zsp1jarocin.edu.pl>
2024-11-21 14:42:05 +0100

    migration potential students

---
+++ database/migrations/2024_11_21_133952_create_potential_students.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('potential_students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->year('birth_year');
+            $table->string('status');
+            $table->longText('comment');
+            $table->string('parent_name');
+            $table->string('parent_surname');
+            $table->string('parent_phone_number');
+            $table->string('parent_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('potential_students');
+    }
+};
