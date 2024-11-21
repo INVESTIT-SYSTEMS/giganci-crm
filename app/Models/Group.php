32897655eb29e0366a0a3fce8ab891a69bfcff71
Jakub Jańczak <jakub.janczak@zsp1jarocin.edu.pl>
2024-11-21 14:20:28 +0100

    model group edited and teacher model add

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,21 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
     protected $fillable = [
         'name',
-        'surname',
-        'birth_year',
-        'parent_name',
-        'parent_surname',
-        'parent_phone_number',
-        'parent_email',
+        'classes_day',
+        'classes_hour',
+        'id_teacher',
+        'id_location',
     ];
     public function student(): HasMany
     {
         return $this->hasMany(Student::class);
+    }
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(Teacher::class);
     }
 }
