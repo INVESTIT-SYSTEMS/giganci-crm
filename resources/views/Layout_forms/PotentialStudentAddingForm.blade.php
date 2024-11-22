e77b454ffc60eee02b3dc371cb9283e440e9541f
Jakub Jańczak <jakub.janczak@zsp1jarocin.edu.pl>
2024-11-22 08:28:33 +0100

    add route and Controller

---
+++ resources/views/Layout_forms/PotentialStudentAddingForm.blade.php
@@ -0,0 +1,6 @@
+@section()
+    <form action="" method="post">
+        @csrf
+        @method('post')
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PotentialStudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('PotentialStudent_routes', PotentialStudentController::class);
