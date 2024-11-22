be621c9b8fba15d1c2d5be4c9187416b247204c8
Zuliet_zsp1 <julia.jedrzejewska@zsp1jarocin.edu.pl>
2024-11-22 08:56:15 +0100

    Teacher adding form, routes and controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PotentialStudentController;
-
+use App\Http\Controllers\TeacherController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('PotentialStudent_routes', PotentialStudentController::class);
+Route::resource('TeacherController_routes', TeacherController::class);
