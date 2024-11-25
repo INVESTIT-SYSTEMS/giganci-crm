4c0ca3cc42603d0408473d1769906645237ae3e3
PerlaZsp1 <bartosz.rojewski@zsp1jarocin.edu.pl>
2024-11-25 13:20:24 +0100

    AddingStudentPack#1

--- resources/views/Layout_forms/PotentialStudentAddingForm.blade.php
+++ resources/views/Layout_forms/PotentialStudentAddingForm.blade.php
@@ -1,10 +1,10 @@
 <head>
-    <title>Edytuj potencjalnego ucznia</title>
+    <title>Dodaj potencjalnego ucznia</title>
 </head>
 @include('Layout_forms.headlayout')
 
 <section class="contentpotentialadd">
-    <h1>Edytuj potencjalnego ucznia</h1>
+    <h1>Dodaj potencjalnego ucznia</h1>
     <section class="potentialuseradd">
         <form action="{{route('addPStudent.store')}}" method="post" id="potential">
             @csrf
@@ -59,26 +59,3 @@
 </section>
 @include('Layout_forms.footerlayout')
 
-
-{{--@section()--}}
-{{--    <form action="{{route('addPStudent.store')}}" method="post" id="potential">--}}
-{{--        @csrf--}}
-
-{{--        <input type="text" name="name" id="">--}}
-{{--        <input type="text" name="surname" id="">--}}
-{{--        <input type="number" name="birth_year" id="">--}}
-{{--        <select name="status" form="potential">--}}
-{{--            <option value="jeden">opcja pierwsza</option>--}}
-{{--            <option value="dwa">opcja druga</option>--}}
-{{--            <option value="trzy">opcja trzecia</option>--}}
-{{--            <option value="cztery">opcja czwarta</option>--}}
-{{--        </select>--}}
-{{--        <textarea form="potential" name="comment"></textarea>--}}
-{{--        <input type="text" name="parent_name" id="">--}}
-{{--        <input type="text" name="parent_surname" id="">--}}
-{{--        <input type="text" name="parent_phone_number" id="">--}}
-{{--        <input type="email" name="parent_email" id="">--}}
-{{--        <button type="submit">Dodaj Użytkownika</button>--}}
-{{--    </form>--}}
-
-{{--@endsection--}}
