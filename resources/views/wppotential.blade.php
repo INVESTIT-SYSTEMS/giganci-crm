b9ba1cba0b2bb325190347d8cb6c977b0600e014
PerlaZsp1 <bartosz.rojewski@zsp1jarocin.edu.pl>
2024-11-25 09:17:28 +0100

    StudentWebPack

--- resources/views/wppotential.blade.php
+++ resources/views/wppotential.blade.php
@@ -22,26 +22,28 @@
                 <th>Edytor</th>
             </tr>
             @foreach($user as $info)
+                <div class="gap">
             <tr>
                 <td>{{$info->id}}</td>
-                <td>{{$info->name}}</td>
+                <td class="colored">{{$info->name}}</td>
                 <td>{{$info->surname}}</td>
-                <td>{{$info->birth_year}}</td>
+                <td class="colored">{{$info->birth_year}}</td>
                 <td>{{$info->status}}</td>
-                <td>{{$info->comment}}</td>
+                <td class="colored">{{$info->comment}}</td>
                 <td>{{$info->parent_name}}</td>
-                <td>{{$info->parent_surname}}</td>
+                <td class="colored">{{$info->parent_surname}}</td>
                 <td>{{$info->parent_phone_number}}</td>
-                <td>{{$info->parent_email}}</td>
+                <td class="colored">{{$info->parent_email}}</td>
                 <td>
                     <a href="{{ route('addingPotential.edit', ['addingPotential' => $info])}}"><button>Edit</button></a> <br>
                     <form action="{{ route('addingPotential.destroy', ['addingPotential' => $info]) }}" method="post">
                         @csrf
                         @method('delete')
-                        <button type="submit">X</button>
+                        <button type="submit" class="del">X</button>
                     </form>
                 </td>
             </tr>
+                </div>
             @endforeach
         </table>
     </section>
--- resources/views/wpstudent.blade.php
+++ resources/views/wpstudent.blade.php
@@ -1,2 +1,44 @@
 @include('Layout_forms.headlayout')
-<h1>Uczniowie</h1>
+<section class="contentstudent">
+    <h1>Uczniowie</h1>
+    <section class="student">
+        <table class="">
+            <tr>
+                <th>Id</th>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>Rok urodzenia</th>
+                <th>Imie rodzica</th>
+                <th>Nazwisko rodzica</th>
+                <th>Numer telefonu rodzica</th>
+                <th>E-mail rodzica</th>
+                <th>Id grupy</th>
+                <th>Edytor</th>
+            </tr>
+            @foreach($user as $info)
+                <div class="gap">
+                    <tr>
+                        <td>{{$info->id}}</td>
+                        <td class="colored">{{$info->name}}</td>
+                        <td>{{$info->surname}}</td>
+                        <td class="colored">{{$info->birth_year}}</td>
+                        <td>{{$info->parent_name}}</td>
+                        <td class="colored">{{$info->parent_surname}}</td>
+                        <td>{{$info->parent_phone_number}}</td>
+                        <td class="colored">{{$info->parent_email}}</td>
+                        <td>{{$info->group_id}}</td>
+                        <td class="colored">
+                            <a href="{{ route('addingPotential.edit', ['addingPotential' => $info])}}"><button>Edit</button></a> <br>
+                            <form action="{{ route('addingPotential.destroy', ['addingPotential' => $info]) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="del">X</button>
+                            </form>
+                        </td>
+                    </tr>
+                </div>
+            @endforeach
+        </table>
+    </section>
+</section>
+@include('Layout_forms.footerlayout')
