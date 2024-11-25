ad4fb7508fe285ddd51e7183c6b94b7b22391e38
PerlaZsp1 <bartosz.rojewski@zsp1jarocin.edu.pl>
2024-11-25 09:51:46 +0100

    GroupWebPack

--- resources/views/wpgroup.blade.php
+++ resources/views/wpgroup.blade.php
@@ -1,2 +1,38 @@
 @include('Layout_forms.headlayout')
-<h1>Grupy</h1>
+<section class="contentgroup">
+    <h1>Grupy</h1>
+    <section class="group">
+        <table class="">
+            <tr>
+                <th>Id</th>
+                <th>Nazwa</th>
+                <th>Dzień</th>
+                <th>Godzina</th>
+                <th>Nauczyciel</th>
+                <th>Lokalizacja</th>
+                <th>Edytor</th>
+            </tr>
+            @foreach($group as $info)
+                <div class="gap">
+                    <tr>
+                        <td>{{$info->id}}</td>
+                        <td class="colored">{{$info->name}}</td>
+                        <td>{{$info->classes_day}}</td>
+                        <td class="colored">{{$info->classes_hour}}</td>
+                        <td>{{$info->teacher_id}}</td>
+                        <td class="colored">{{$info->location_id}}</td>
+                        <td>
+                            <a href="{{ route('addingPotential.edit', ['addingPotential' => $info])}}"><button>Edit</button></a> <br>
+                            <form action="{{ route('addingPotential.destroy', ['addingPotential' => $info]) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="del">X</button>
+                            </form>
+                        </td>
+                    </tr>
+                </div>
+            @endforeach
+        </table>
+    </section>
+</section>
+
