4f40359ec837e67e6895f0c1c1d727ee1ef7e4eb
PerlaZsp1 <bartosz.rojewski@zsp1jarocin.edu.pl>
2024-11-27 10:02:04 +0100

    FooterPack#2

--- resources/views/Layout_forms/footerlayout.blade.php
+++ resources/views/Layout_forms/footerlayout.blade.php
@@ -1,4 +1,10 @@
-<footer class="bg-gray-500">
-    <p>Strone wykonali: Praktykanci </p>
-    <p> Mentalnie nie mam jeszcze na to pomysłu</p>
+<footer>
+    <p>&copy; 2024 Copyright: InvestIT</p>
+    <div class="waves">
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave2"></div>
+        <div class="wave" id="wave3"></div>
+        <div class="wave" id="wave4"></div>
+    </div>
+
 </footer>
