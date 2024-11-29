3b261a35536750b1108f11ed153ec1c541b48f03
Jakub Jańczak <jakub.janczak@zsp1jarocin.edu.pl>
2024-11-29 11:34:11 +0100

    lokalizacja w uczniu

--- resources/views/wpstudent.blade.php
+++ resources/views/wpstudent.blade.php
@@ -52,6 +52,7 @@
                     <th>Numer telefonu rodzica</th>
                     <th>E-mail rodzica</th>
                     <th>Nazwa grupy</th>
+                    <th>Lokalizacaja</th>
                     <th>Edytor</th>
                 </tr>
 
@@ -67,6 +68,7 @@
                                 <td>{{$info->parent_phone_number}}</td>
                                 <td class="colored">{{$info->parent_email}}</td>
                                 <td>{{$info->group ? $info->group->name:'Brak grupy'}}</td>
+                                <td>{{$info->group->location->town}}</td>
                                <td class="colored">
                                     <a href="{{ route('students.edit', ['student' => $info])}}"><button>Edit</button></a> <br>
                                     <form action="{{ route('students.destroy', ['student' => $info]) }}" method="post">
