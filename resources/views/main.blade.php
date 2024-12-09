72271cb633a3c32b749ebeb12adc4cc7c1421cb2
PerlaZsp1 <bartosz.rojewski@zsp1jarocin.edu.pl>
2024-12-09 08:56:09 +0100

    MessageAfterSend#1

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,15 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Giganci Programowania</title>
     @vite('resources/css/app.css')
+
 </head>
 <body>
 @include('Layout_forms.headlayout')
 <main class="w-100">
     <h1>Giganci programowania CRM</h1>
     <h3>Siema Marcin</h3>
-    @if(session('send'))
-        <p>{{session('send')}}</p>
-    @endif
+
+    <section class="modal-active" id="modal">
+        @if(session('send'))
+            <p>{{session('send')}}</p>
+        @endif
+        <button class="close" type="button" id="close">X</button>
+    </section>
 
     <section class="bg-gray-300 contentmain">
         <a href="{{ route('students.index') }}"><div class="maindiv">
@@ -41,5 +46,16 @@
 </main>
 @include('Layout_forms.footerlayout')
 
+
+<script>
+    const buttonClose = document.getElementById("close");
+    let windowClose = document.getElementById("modal");
+    buttonClose.addEventListener("click", () => {
+        windowClose.setAttribute("class", "modal-active");
+    });
+</script>
 </body>
 </html>
+
+
+
