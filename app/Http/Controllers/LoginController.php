17c3511226f45a3ecdb1a9e21291d4a5d824c01a
Zuliet_zsp1 <julia.jedrzejewska@zsp1jarocin.edu.pl>
2024-12-12 12:14:14 +0100

    Login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('wplogin');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+//        $data = $request->validate([
+//        'login'=>'required',
+//        'password'=>'required',
+//    ]);
+
+        $data = $request->only([
+            'login',
+            'password'
+        ]);
+
+        if (Auth::attempt($data)){
+            $request->session()->regenerate();
+            return redirect()->intended(route('main.index'));
+        }
+        return view('wplogin');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
